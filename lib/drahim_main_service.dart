e3f56efcf73ee46a11e37429baf50c605454eb62
aghaleb <henno.noman@gmail.com>
2025-04-16 11:33:16 +0300

    add id

--- lib/drahim_main_service.dart
+++ lib/drahim_main_service.dart
@@ -2,16 +2,19 @@ import 'package:drahim_services/drahim_service.dart';
 import 'package:drahim_services/label.dart';
 
 class DrahimMainService {
+  final int id;
   final Label title;
   final List<DrahimService> services;
 
   const DrahimMainService({
+    required this.id,
     required this.title,
     required this.services,
   });
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'title': title.toJson(),
       'sub_services': services
           .map(
@@ -23,9 +26,11 @@ class DrahimMainService {
 
   static final data = [
     DrahimMainService(
+      id: 1,
       title: Label(en: 'Investment Services', ar: 'خدمات الاستثمار'),
       services: [
         DrahimService(
+          id: 1,
           title: Label(en: 'Investment Calculator', ar: 'حاسبة الاستثمار'),
           description: Label(
             en: 'See how your investments grow over the years',
@@ -33,16 +38,16 @@ class DrahimMainService {
           ),
         ),
         DrahimService(
+          id: 2,
           title: Label(
-            en: 'Link External Portfolio',
-            ar: 'ربط محفظة استثمارية خارجية',
-          ),
+              en: 'Link External Portfolio', ar: 'ربط محفظة استثمارية خارجية'),
           description: Label(
             en: 'Track your external investments seamlessly',
             ar: 'تابع استثماراتك الخارجية بكل سهولة',
           ),
         ),
         DrahimService(
+          id: 3,
           title: Label(en: 'Custom Portfolios', ar: 'تخصيص المحافظ'),
           description: Label(
             en: 'Design your portfolio to match your expertise and goals',
@@ -50,6 +55,7 @@ class DrahimMainService {
           ),
         ),
         DrahimService(
+          id: 4,
           title: Label(en: 'Investment Goals', ar: 'الأهداف الاستثمارية'),
           description: Label(
             en: 'Set your goals and plan step by step to achieve them',
@@ -57,6 +63,7 @@ class DrahimMainService {
           ),
         ),
         DrahimService(
+          id: 5,
           title: Label(en: 'Monthly Investments', ar: 'الاستقطاع الشهري'),
           description: Label(
             en: 'Enable monthly deductions to meet your investment goals with ease',
@@ -64,6 +71,7 @@ class DrahimMainService {
           ),
         ),
         DrahimService(
+          id: 6,
           title: Label(
               en: 'Create Investment Portfolio', ar: 'فتح محفظة استثمارية'),
           description: Label(
@@ -74,10 +82,12 @@ class DrahimMainService {
       ],
     ),
     DrahimMainService(
+      id: 2,
       title:
           Label(en: 'Expense Management Services', ar: 'خدمات متابعة المصاريف'),
       services: [
         DrahimService(
+          id: 7,
           title: Label(en: 'Budgets', ar: 'أنشئ ميزانية'),
           description: Label(
             en: 'Plan weekly, monthly, or annual budgets effortlessly',
@@ -85,16 +95,15 @@ class DrahimMainService {
           ),
         ),
         DrahimService(
-          title: Label(
-            en: 'Create Manual Account',
-            ar: 'إضافة حساب يدوي',
-          ),
+          id: 8,
+          title: Label(en: 'Create Manual Account', ar: 'إضافة حساب يدوي'),
           description: Label(
             en: 'Manage all your bills and loans from one place',
             ar: 'تابع جميع فواتيرك وقروضك من مكان واحد',
           ),
         ),
         DrahimService(
+          id: 9,
           title: Label(en: 'Link Bank Account', ar: 'ربط حساب بنكي'),
           description: Label(
             en: 'Track your bank transactions securely in real time',
@@ -102,6 +111,7 @@ class DrahimMainService {
           ),
         ),
         DrahimService(
+          id: 10,
           title: Label(en: 'Manage Commitments', ar: 'إدارة الالتزامات'),
           description: Label(
             en: 'Netflix, YouTube, or any other subscriptions',
@@ -111,9 +121,11 @@ class DrahimMainService {
       ],
     ),
     DrahimMainService(
+      id: 3,
       title: Label(en: 'General Services', ar: 'خدمات عامة'),
       services: [
         DrahimService(
+          id: 11,
           title: Label(en: 'Asset Management', ar: 'إدارة الأصول'),
           description: Label(
             en: 'Track your money, properties, gold, and more.',
@@ -121,16 +133,15 @@ class DrahimMainService {
           ),
         ),
         DrahimService(
-          title: Label(
-            en: 'Drahim GPT',
-            ar: 'دراهم GPT',
-          ),
+          id: 12,
+          title: Label(en: 'Drahim GPT', ar: 'دراهم GPT'),
           description: Label(
             en: 'Ask Drahim anything using AI',
             ar: 'اسأل دراهم أي سؤال باستخدام الذكاء الصناعي',
           ),
         ),
         DrahimService(
+          id: 13,
           title: Label(en: 'Drahim Plus', ar: 'دراهم بلس'),
           description: Label(
             en: 'Unlock exclusive features and extra benefits with your subscription',
@@ -138,6 +149,7 @@ class DrahimMainService {
           ),
         ),
         DrahimService(
+          id: 14,
           title:
               Label(en: 'Drahim Private Membership', ar: 'عضوية دراهم الخاصة'),
           description: Label(
@@ -146,6 +158,7 @@ class DrahimMainService {
           ),
         ),
         DrahimService(
+          id: 15,
           title: Label(en: 'Zakat Calculator', ar: 'حاسبة الزكاة'),
           description: Label(
             en: 'Easily calculate and fulfill your zakat accurately',
@@ -153,6 +166,7 @@ class DrahimMainService {
           ),
         ),
         DrahimService(
+          id: 16,
           title: Label(en: 'Invite your friends!', ar: 'اعزم معارفك!'),
           description: Label(
             en: 'Free Drahim Plus for both of you',
--- lib/drahim_service.dart
+++ lib/drahim_service.dart
@@ -1,26 +1,21 @@
 import 'package:drahim_services/label.dart';
 
 class DrahimService {
+  final int id;
   final Label title;
   final Label description;
 
   const DrahimService({
+    required this.id,
     required this.title,
     required this.description,
   });
 
-  factory DrahimService.fromJson(Map<String, dynamic> json) {
-    return DrahimService(
-      title: json['title'],
-      description: json['description'],
-    );
-  }
-
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'title': title.toJson(),
       'description': description.toJson(),
     };
   }
-
 }
